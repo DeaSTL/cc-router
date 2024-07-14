31e6f02a622a327a04ed4b37dbdf291041373792
Jon-Michael Hartway <jhartway99@gmail.com>
2024-07-14 17:51:10 -0500

    update

--- main.lua
+++ main.lua
@@ -7,13 +7,13 @@ local config = {
 
 function formatIP(ipInt)
   local block1 = bit32.band(0xFF000000)
-  block1 = bit32.rshift(block1,24)
+  block1 = bit32.lshift(block1,24)
 
   local block2 = bit32.band(0x00FF0000)
-  block2 = bit32.rshift(block1,16)
+  block2 = bit32.lshift(block1,16)
 
   local block3 = bit32.band(0x0000FF00)
-  block3 = bit32.rshift(block1,8)
+  block3 = bit32.lshift(block1,8)
 
   local block4 = bit32.band(0x000000FF)
 

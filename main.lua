1c0e0a83cc60a692c43f9c8e788730b388c8f070
Jon-Michael Hartway <jhartway99@gmail.com>
2024-07-14 17:50:01 -0500

    update

--- main.lua
+++ main.lua
@@ -1,4 +1,31 @@
 
+
+local config = {
+  ip=0xC0A80001
+}
+
+
+function formatIP(ipInt)
+  local block1 = bit32.band(0xFF000000)
+  block1 = bit32.rshift(block1,24)
+
+  local block2 = bit32.band(0x00FF0000)
+  block2 = bit32.rshift(block1,16)
+
+  local block3 = bit32.band(0x0000FF00)
+  block3 = bit32.rshift(block1,8)
+
+  local block4 = bit32.band(0x000000FF)
+
+  print(
+  block1.. "," ..
+  block2.. "," ..
+  block3.. "," ..
+  block4
+  )
+end
+
+
 function getModem()
 
   local peripherals = peripheral.getNames()

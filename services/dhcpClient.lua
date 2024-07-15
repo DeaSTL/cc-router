3fa32ee4286a9cdb6d3ec2b00aeae07e5235294a
Jon-Michael Hartway <jhartway99@gmail.com>
2024-07-14 22:35:41 -0500

    update

--- services/dhcpClient.lua
+++ services/dhcpClient.lua
@@ -1,9 +1,10 @@
 dhcpClientService = {}
 
 function dhcpClientService:init()
+  print("Running dhcp client service")
 end
 
 function dhcpClientService:run()
-
+  print("Running dhcp client service")
 end
 

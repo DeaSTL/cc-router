349122e3b64d1b56981995dba0e1938b559566f4
Jon-Michael Hartway <jhartway99@gmail.com>
2024-07-14 22:15:59 -0500

    update

--- router.lua
+++ router.lua
@@ -2,13 +2,13 @@ require("iplib")
 require("services")
 
 
-local router = {
+local config = {
   ip=iplib:ipStrToInt("192.168.1.1"),
   ipRange=iplib:cidrStrToMaskInt("192.168.1.0/24"),
 }
 
 
-function router:generateIpRange()
+function config:generateIpRange()
   local maxRange = bit32.bnot(self.ipRange)
   local ipPrefix = bit32.band(self.ip, self.ipRange)
   for i = 0, maxRange, 1 do
@@ -19,17 +19,17 @@ end
 
 
 
-print("ipstr:"..iplib:ipIntToStr(router.ip))
+print("ipstr:"..iplib:ipIntToStr(config.ip))
 print("masked ip:"..
   iplib:ipIntToStr(
     bit32.band(
-      bit32.bnot(router.ipRange),
-      router.ip
+      bit32.bnot(config.ipRange),
+      config.ip
     )
   )
 )
 
-startServices(router)
+startServices(config)
 -- print("inverted range: ",bit32.bnot(router.ipRange))
 -- router:generateIpRange()
 -- getModem()

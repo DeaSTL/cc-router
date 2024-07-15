ba096b280732645f60304cb60ff534c95d14485e
Jon-Michael Hartway <jhartway99@gmail.com>
2024-07-14 21:38:11 -0500

    update

--- services/rednet.lua
+++ services/rednet.lua
@@ -1,7 +1,25 @@
 rednetService = {}
 
+
+function setupModem()
+
+  local peripherals = peripheral.getNames()
+
+  for i=1,#peripherals do
+    per = peripherals[i]
+    perType = peripheral.getType(per)
+    if perType == 'modem' then 
+      print("found modem at " .. per)
+      rednet.open(per)
+      return
+    end
+  end
+  error("Could not find modem")
+end
+
 function rednetService:init()
   print("Initializing rednet service")
+  setupModem()
 end
 function rednetService:run()
   print("Running rednet service")

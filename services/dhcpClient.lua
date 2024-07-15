871c015b779107c57e1f72d0fd0c1794d14054fc
Jon-Michael Hartway <jhartway99@gmail.com>
2024-07-14 22:43:40 -0500

    update

--- services/dhcpClient.lua
+++ services/dhcpClient.lua
@@ -1,8 +1,27 @@
 dhcpClientService = {}
 
+function requestNewIP()
+  local dhcpPayload = {
+    type = "DHCP_NEW"
+
+  }
+  local request = {
+
+  }
+  rednet.send
+end
+
 function dhcpClientService:init()
   print("Initializing dhcp client service")
-  
+  local dhcpIP = settings.get("dhcp.ip")
+
+  if dhcpIP == nil then
+    
+  end
+  ---@type requestFmt
+  local request = {
+    src = 
+  }
   rednet.send()
 end
 
